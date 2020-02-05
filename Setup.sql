27e8f40b6d409c4b64bb2bac41cbc5e43f149c4e
Jenny <ricejl@gmail.com>
2020-02-05 15:36:48 -0700

    add flowers table

---
+++ Setup.sql
@@ -0,0 +1,9 @@
+USE flowery;
+
+-- CREATE TABLE flowers (
+--     id int NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(255) NOT NULL,
+--     price DECIMAL(5, 2) NOT NULL,
+--     perennial BIT DEFAULT 0,
+--     PRIMARY KEY (id)
+-- )
